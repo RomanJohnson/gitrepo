9f15fc0d1f5488c28a358121427a78d5ad55dc80
github username <github email address>
2017-07-07 17:43:19 -0400

    working draft

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,38 +1,70 @@
+require "pry"
+gem 'random-word', '~> 1.3'
 
-def hangman
+class Hangman
 
+  def initialize
+    @count = 0
+    @win_count = 0
+    @loss_count = 0
+    @game_over = 0
 
-  def create_word
-    word = "hangman"
-    template = "_"*word.length
+    create_word
   end
-  def prompt
-    puts "Guess a character a-z!"
-    guess = gets.chomp
-  end
-  count = 0
-  progress = template.chars
 
-  def play
-    word.chars.each_with_index do |letter, index|
-    if letter == guess
-      progress[index] = letter
-    else
-        count += 1
+    def create_word
+      @count = 0
+      @word = RandomWord.nouns.next
+      template = "_"* @word.length
+      @progress = template.chars
+      @game_over = 0
+      prompt
     end
 
-    if count == 5
-      puts "You Lose!"
+    def prompt
+      display
+      puts "Guess a character a-z!"
+      guess = gets.chomp
+      play(guess)
     end
 
-    if progress.join == word
-      puts "You Win!"
+    def play(guess)
+      flag = 0
+      @word.chars.each_with_index do |letter, index|
+          if letter == guess
+            @progress[index] = letter
+            flag = 1
+          end
+      end
+        if flag == 0
+        @count += 1
+        end
+      progress_check
     end
-  end
+
+    def progress_check
+
+      if @count >= 5
+        puts "You Lose!"
+        @loss_count += 1
+        create_word
+        display
+      elsif @progress.join == @word
+        puts "You Win!"
+        @win_count += 1
+        display
+        create_word
+      else
+        prompt
+      end
+    end
 
 
     def display
-        progress
+      print  @progress.join
+      puts
     end
 
+
 end
+Hangman.new
